6e1bcd839a92b51b08fddccf14f204565ce82fe6
Sierra Janson <60485703+sierrajanson@users.noreply.github.com>
2024-03-04 13:33:55 -0800

    removed unnecessary imports and components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
-import Contact from './views/Contact';
 import Home from './views/Home';
-import {Link, useNavigate, BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 function App() {
-
-  // navigation system
-  // header
-  // tagline 
-
-  // circles for design
-  // pen componenet
   return (
     <div className="App">
       <Router>
           <Navbar/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
-                <Route path='/contact' element={<Contact/>} />
-
             </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './Navbar.css'
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar= () =>{
-  const [loggedIn, setLoggedIn] = useState(false);
-  const navigate = useNavigate();
 
   return (
   <div className="link_container">
@@ -16,7 +14,7 @@ const Navbar= () =>{
       <Link to="/"className="link">Home</Link>
     </li>
     <li>
-      <Link to="/contact"className="link">Contact</Link>
+      <Link to="/"className="link">Contact</Link>
     </li>
     <li>
     <Link to="/home"className="link">Language</Link>
